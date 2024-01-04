71060a47ed753367c053dd25c3cfc9be6a303e60
Bruno Martins <bgcngm@gmail.com>
2024-01-04 23:13:26 +0000

    ims-ext: Restore getLteDataUsage method for backward compatibility
    Removed in commit c6c474c4ee331a1c0eb1b54f7f9d515a7a7af21e,
    but still needed for old prebuilt apks.
    
    Change-Id: I1f55d247a07b65d2a7b45b88887bf05185a02a2e

--- ims/ims-ext-common/src/org/codeaurora/ims/QtiVideoCallDataUsage.java
+++ ims/ims-ext-common/src/org/codeaurora/ims/QtiVideoCallDataUsage.java
@@ -63,6 +63,13 @@ public class QtiVideoCallDataUsage implements Parcelable {
     }
 
     /*
+     * This method returns LTE Data Usage (for backward compatibility)
+     */
+    public long getLteDataUsage() {
+        return getWwanDataUsage();
+    }
+
+    /*
      * This method returns WWAN Data Usage
      */
     public long getWwanDataUsage() {
